1485aa3d3ea78e7cafea523f5cbc38c13df0f2fd
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2022-01-25 10:12:47 -0500

    clean up

--- exercise.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("sentence_dataset.csv", newline='') as csvfile:
-    sentences = csv.reader(csvfile, delimiter=',')
-    for row in sentences:
-        print(row)
--- pandas/pandas.py
+++
@@ -1,8 +0,0 @@
-import json
-import pandas as pd
-
-with open('sentence_dataset.csv') as f_input:
-    df = pd.read_json(f_input)
-
-df = df.bfill(axis='columns')
-df.iloc[:, 0].to_csv('some.csv', encoding='utf-8', header=False)
